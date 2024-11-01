2b14d771c28f52ce40918631a17ec46e2d9c489b
Yxtomix <yxtomix@gmail.com>
2024-11-01 14:42:07 +0100

    Update

--- config/database.js
+++ config/database.js
@@ -1,26 +1,18 @@
 import pg from 'pg';
 import { Sequelize } from 'sequelize';
 
-//const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE,          // Nom de la base de données
-  process.env.POSTGRES_USER,              // Utilisateur de la base de données
-  process.env.POSTGRES_PASSWORD,          // Mot de passe de la base de données
-  {
-    host: process.env.POSTGRES_HOST,      // Hôte de la base de données
-    dialect: 'postgres',
-    dialectModule: pg,                  // Dialecte de Sequelize
-    port: process.env.POSTGRES_PORT || 6543, // Port de la base de données (défaut 6543 si non défini)
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,       // Désactiver la vérification SSL pour éviter les erreurs
-      },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,  
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 module.exports = sequelize;
