a9ff3a4d5d29095254e3692a73a56cad34e39817
Yxtomix <yxtomix@gmail.com>
2024-11-01 14:06:00 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 
 const cors = require('cors');
-const User = require('./models/User');
-const Clinic = require('./models/Clinic');
-const Animal = require('./models/Animal');
-const Appointment = require('./models/Appointment');
-const authRoutes = require('./routes/auth');
-const clinicRoutes = require('./routes/ClinicCrud');
-const animalRoutes = require('./routes/animalRoutes');
-const timeSlotRoutes = require('./routes/timeSlotRoutes');
-const appointmentRoutes = require('./routes/appointmentRoutes');
 
 require('dotenv').config();
 const app = express();
